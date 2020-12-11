81364f4a08e6aafd800b37ec433dcc447c4ad82b
luyibiao <lu.yibiao@hb-cloud.cn>
2020-12-11 15:11:55 +0800

    增加组件script标签引入方式

--- src/index.js
+++ src/index.js
@@ -4,15 +4,22 @@ import Prototypes from './prototypes'
 import utils from './utils'
 import './styles/index.scss'
 
+// 定义 install 方法
+const install = function (Vue, options = {}) {
+  // 注册组件
+  Vue.use(Components)
+  // 注册指令
+  Vue.use(Directives)
+  // 注册实例方法
+  Vue.use(Prototypes, { scope: options.scope })
+  // 注册全局方法
+  Vue.use(utils)
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
 export default {
-  install: (Vue, options = {}) => {
-    // 注册组件
-    Vue.use(Components)
-    // 注册指令
-    Vue.use(Directives)
-    // 注册实例方法
-    Vue.use(Prototypes, { scope: options.scope })
-    // 注册全局方法
-    Vue.use(utils)
-  }
+  install
 }
